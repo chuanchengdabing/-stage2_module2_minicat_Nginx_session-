651a003cf51b58c510b1694ab12fb74df279412e
chuanchengdabing <603202711@qq.com>
2020-07-01 16:52:20 +0800

    阶段二，模块二资料上传

---
+++ solution_work/src/main/java/com/lagou/service/ResumeService.java
@@ -0,0 +1,15 @@
+package com.lagou.service;
+
+import com.lagou.pojo.Resume;
+
+import java.util.List;
+
+public interface ResumeService {
+    List<Resume> queryResumeList() throws Exception;
+
+    Resume queryResumeById(Integer id);
+
+    void updateResumeById(Resume resume);
+
+    void deleteResumeById(long longValue);
+}
